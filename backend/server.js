e1f485ff4d226b52d04219fbc46a97ed66d25fa7
Hack <hopewhite.gamer@gmail.com>
2026-01-20 14:28:15 -0700

    Change server port from 3000 to 10000

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
